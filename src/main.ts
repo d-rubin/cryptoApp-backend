1a807f023eaaf98292086674f12131f493bb53fa
Daniel Rubin <daniel.rubin1997@gmail.com>
2025-02-13 18:47:49 +0100

    feat: configure cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: process.env.ALLOWED_ORIGIN },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGIN,
   });
   await app.listen(process.env.PORT || 8000);
 }
